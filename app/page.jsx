bf2cef25e5f8fe42a6e24592ebeea340367dab22
Gibson Joseph <gibsonjoseph890@gmail.com>
2023-06-21 16:58:09 +0530

    feat: change heading name

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI-Powred Prompts</span>
+        <span className="orange_gradient text-center">Yavar Blog Application</span>
       </h1>
       <p className="desc text-center">
         Promtopia is an open-source AI prompting tool for modern world to
